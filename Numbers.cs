93452f915bfb8e490c27bcd0b345116b290845fc
DavidVerdianto <61915681+zerefsamaxix@users.noreply.github.com>
2022-04-09 19:49:25 +0700

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum (int number1, int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int IntNumber3 = (int)number3;
+            return Math.Min(number1, Math.Min(number2, IntNumber3));
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int IntNumber3 = (int)number3;
+            return Math.Max(number1, Math.Max(number2, IntNumber3));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine("Minimum #1 : {0}", numbers.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2 : {0}", numbers.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1 : {0}", numbers.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2 : {0}", numbers.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
